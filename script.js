18cd1a1655772bdd4b22bbabf8e9b5fce87712d6
abirtasrif <abirtasrif@gmail.com>
2022-10-11 23:32:51 +0600

    finalizing

--- script.js
+++ script.js
@@ -135,3 +135,88 @@ const imageObserver = new IntersectionObserver(loadImage, {
 });
 
 imgTargets.forEach(img => imageObserver.observe(img));
+
+// button handles
+btnLeft.addEventListener("click", previousSlide);
+btnRight.addEventListener("click", nextSlide);
+
+// Slider
+let currentSlide = 0;
+const maxSlide = slides.length - 1;
+
+// DOts
+function creatingDots() {
+  slides.forEach((_, i) => {
+    const dot = `<button class="dots__dot" data-slide="${i}"></button>`;
+    dotContainer.insertAdjacentHTML("beforeend", dot);
+  });
+}
+
+creatingDots();
+
+// Activate dots
+function activateDots(slide) {
+  document
+    .querySelectorAll(".dots__dot")
+    .forEach((dot) => dot.classList.remove("dots__dot--active"));
+
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add("dots__dot--active");
+}
+
+activateDots(0);
+
+function updateSlide(cs) {
+  slides.forEach(
+    (sl, i) => (sl.style.transform = `translateX(${100 * (i - cs)}%)`)
+  );
+}
+
+updateSlide(0);
+
+function previousSlide() {
+  if (currentSlide === 0) currentSlide = maxSlide;
+  else currentSlide--;
+  updateSlide(currentSlide);
+  activateDots(currentSlide);
+}
+
+function nextSlide() {
+  if (currentSlide === maxSlide) currentSlide = 0;
+  else currentSlide++;
+  updateSlide(currentSlide);
+  activateDots(currentSlide);
+}
+
+// dots handler
+dotContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("dots__dot")) {
+    activateDots(e.target.dataset.slide);
+    updateSlide(e.target.dataset.slide);
+  }
+});
+
+
+// keyboard arrow
+document.addEventListener("keydown", (e) => {
+    e.key === "ArrowLeft" && previousSlide();
+    e.key === "ArrowRight" && nextSlide();
+  });
+  
+  // tabbed components
+  tabsContainer.addEventListener("click", function (e) {
+    const btn = e.target.closest(".operations__tab");
+  
+    if (!btn) return;
+  
+    tabs.forEach((tab) => tab.classList.remove("operations__tab--active"));
+    tabsContent.forEach((content) =>
+      content.classList.remove("operations__content--active")
+    );
+  
+    btn.classList.add("operations__tab--active");
+    document
+      .querySelector(`.operations__content--${btn.dataset.tab}`)
+      .classList.add("operations__content--active");
+  });
