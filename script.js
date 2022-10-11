e904879e9741ec08a9a7fda66284febe799b5d42
abirtasrif <abirtasrif@gmail.com>
2022-10-11 20:14:44 +0600

    updating more

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/////////////////////////////////////////////////////////////
+// Elements
+/////////////////////////////////////////////////////////////
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const nav = document.querySelector(".nav");
+const navLinks = document.querySelector(".nav_links");
+const header = document.querySelector(".header");
+const toggleBtn = document.querySelector(".nav_toggle");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const allSections = document.querySelectorAll(".section");
+const section1 = document.querySelector("#section--1");
+const tabsContainer = document.querySelector(".operations_tab-container");
+const tabs = document.querySelectorAll(".operations_tab");
+const tabsContent = document.querySelectorAll(".operations_content");
+const cookieBody = document.querySelector(".cookie");
+const cookieCloseBtn = document.querySelector(".cookie_close");
+const imgTargets = document.querySelectorAll("img[data-src]");
+const slides = document.querySelectorAll(".slide");
+const slider = document.querySelector(".slider");
+const btnLeft = document.querySelector(".slider_btn--left");
+const btnRight = document.querySelector(".slider_btn--right");
+const dotContainer = document.querySelector(".dots");
+
+
